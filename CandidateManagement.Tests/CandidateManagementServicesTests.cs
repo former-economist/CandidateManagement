a8cc7e87dd0988e2dd5d8a8c11e06530f68ac90c
adam <adam.smith97@btinternet.com>
2025-01-22 11:18:56 +0000

    added initial testing files

---
+++ CandidateManagement.Tests/CandidateManagementServicesTests.cs
@@ -0,0 +1,11 @@
+﻿namespace CandidateManagement.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
